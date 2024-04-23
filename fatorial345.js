2d526c0e478a83ef2c4e682f86ef0691e572d5d0
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-04-23 10:21:21 -0300

    Create fatorial345.js

---
+++ fatorial345.js
@@ -0,0 +1,10 @@
+const fatorial = 345;
+let i = 0;
+let resultdo = 0;
+
+for (i = 0 ; i < fatorial; i++ ){
+
+    resultdo = resultdo *(fatorial -i)
+    
+    console.log(resultdo + "= (" + resultdo +" * (" +fatorial+ "-" i +");
+}
