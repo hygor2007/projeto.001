e1f772aed92bcfca7081eaa49f36e370e74ac377
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-05-13 08:56:27 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,19 +1,27 @@
 import random
 continuar  = "S"
+Perro = 0 
+Pcerto = 0
 
-while continuar.upper == "S" :
+while continuar.upper () == "S" :
     ns = random.randint(1,10)
 
-    t = 3
+    T = 3
 
-    while( t > 0):
-        print(" voce tem" , t, "tentativa")
-        t = t -1
+    while( T > 0):
+        print(" voce tem" , T, "tentativa")
+        T = T -1
 
         nc = int(input ("Digite um Número de 1 a 10: "))
         if (ns == nc):
             print("você acertou.")
+            T = 0
+            Pcerto = Pcerto + 1 
         else:
             print("você errou.")
+            Perro = Perro + 1
+        print("numero de acerto:", Pcerto)
+        print("numero  de erros:", Perro)
+
 
 continuar = input("deseja continuar (S)im")
